e8b3e1b8a924f19d20cef3aa68ada0caaeaffc49
BeruboIV <hardikraj13@gmail.com>
2020-12-10 00:29:30 +0530

    Handled the case when par = -1 because level[-1] could give errors

--- Graphs/Lowest Common Ancestor.cpp
+++ Graphs/Lowest Common Ancestor.cpp
@@ -1,20 +1,26 @@
-const int N = 1e5;	//Number of nodes.
+const int N = 2e5;	//Number of nodes.
 const int P = 25; //Highest set bit possible
-int LCA[N+1][P+2];
-vector<int> adj[N+1];
-int LOG2[N+1];
-int level[N+1];
+int LCA[N + 1][P + 2]; //LCA[i][j] stores the 2^(jth) parent of node i
+vector<int> adj[N + 1];
+int LOG2[N + 1];
+int level[N + 1];
 
 void dfs(int node, int par){
 	LCA[node][0] = par;
-	level[node] = level[par] + 1;
+	if(par == -1){
+		level[node] = 0;
+	}
+	else{
+		level[node] = level[par] + 1;
+	}
 	for(int child : adj[node]){
 		if(child != par)
 			dfs(child, node);
 	}
 }
 
-void precompute_LCA(int n){
+//We will root the tree at src 
+void precompute_LCA(int src, int n){
 	for(int i = 2; i <= N; i++)
 		LOG2[i] = LOG2[i/2] + 1;
 	int p = LOG2[n];
@@ -22,7 +28,7 @@ void precompute_LCA(int n){
 		for(int j = 0; j <= p; j++)
 			LCA[i][j] = -1;
 	}
-	dfs(1, -1);
+	dfs(src, -1);
 	for(int j = 1; j <= P; j++){
 		for(int i = 1; i <= N; i++){
 			int par = LCA[i][j-1];
@@ -32,19 +38,21 @@ void precompute_LCA(int n){
 				LCA[i][j] = LCA[par][j-1];
 		}
 	}
-	//Let's precalculate Log values also
 	
 }
 
 int getLCA(int a, int b){
-	if(level[b] < level[a])
+	//We need the deeper node
+	//Assume a is the deeper node
+	if(level[a] < level[b])
 		swap(a,b);
-	int d = level[b] - level[a];
+	int d = level[a] - level[b];
 	while(d > 0){
 		int i = LOG2[d];
-		b = LCA[b][i];
+		a = LCA[a][i];
 		d -= (1 << i);
 	}
+	//If a is in the subtree of b
 	if(a == b)
 		return a;
 	// while(LCA[a][0] != LCA[b][0]){
@@ -52,7 +60,7 @@ int getLCA(int a, int b){
 	// 	b = LCA[b][0];
 	// } Takes O(N) time
 	for(int i = P; i >= 0; i--){
-		if(LCA[a][i] != -1 && (LCA[a][i] != LCA[b][i])){
+		if(LCA[b][i] != -1 && (LCA[a][i] != LCA[b][i])){
 			a = LCA[a][i];
 			b = LCA[b][i];
 		}
