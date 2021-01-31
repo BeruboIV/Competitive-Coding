6398521927cf607226d8c859a1469be2ce3f2169
BeruboIV <hardikraj13@gmail.com>
2021-01-31 22:31:49 +0530

    Added DSU

--- Data Structures/DSU.cpp
+++ Data Structures/DSU.cpp
@@ -1,41 +1,30 @@
-const int N = 1e5;
-int parent[N + 1], sz[N + 1];
+const int N = 6e5 + 10;
+int parent[N], sz[N];
 
-int findParent(int a){
-	vector<int> nodes;
-	while(parent[a] > 0){
-		nodes.push_back(a);
-		a = parent[a];
-	}
-
-	for(int node : nodes)
-		parent[node] = a;
-	return a;
+int findParent(int i){
+	if(parent[parent[i]] != parent[i])
+		parent[i] = findParent(parent[i]);
+	return parent[i];
 }
 
-void unionNodes(int a, int b){
+void unionNodes(int a,int b){
 	int parent_a = findParent(a);
 	int parent_b = findParent(b);
-
 	if(parent_a == parent_b)
 		return;
 
-	if(sz[parent_a] > sz[parent_b]){
-		//a would be parent
-		parent[parent_b] = parent_a;
-		sz[parent_a] += sz[parent_b];
-	}
-	else{
-		//b would be parent
-		parent[parent_a] = parent_b;
-		sz[parent_b] += sz[parent_a];
+	if(sz[parent_a] >= sz[parent_b]){
+		swap(parent_a,parent_b);
 	}
 
+	sz[parent_b] += sz[parent_a];
+	parent[parent_a] = parent_b;
+	return;
 }
 
 void clearDSU(int n){
 	for(int i = 0; i <= n; i++){
-		parent[i] = -1;
+		parent[i] = i;
 		sz[i] = 1;
 	}
 }
@@ -53,4 +42,10 @@ void solve(){
 	}
 	for(int i = 1; i <= n; i++)
 		cout << parent[i] << " ";
+	cout << "\n";
+	// For printing the size of each component
+	for(int i = 1; i <= n; i++){
+		int p = parent[i];
+		cout << sz[p] << " ";
+	}
 }
