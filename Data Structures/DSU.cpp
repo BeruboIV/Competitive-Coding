b8e4999f3ab498da8124b0e2089a5c57d7b14cfa
BeruboIV <hardikraj13@gmail.com>
2021-01-31 22:52:19 +0530

    Added recursive and iterative version for find() when parent = -1

--- Data Structures/DSU.cpp
+++ Data Structures/DSU.cpp
@@ -1,24 +1,52 @@
 const int N = 6e5 + 10;
 int parent[N], sz[N];
 
+/* If parent[i] = -1 for all i <= N
+int findParent(int a){
+	vector<int> nodes;
+	while(parent[a] > 0){
+		nodes.push_back(a);
+		a = parent[a];
+	}
+
+	for(int node : nodes)
+		parent[node] = a;
+	return a;
+}
+
+OR
+
+int findParent(int a){
+	if(parent[a] == -1)
+		return a;
+	return parent[a] = findParent(parent[a]);
+}
+*/
+
 int findParent(int i){
 	if(parent[parent[i]] != parent[i])
 		parent[i] = findParent(parent[i]);
 	return parent[i];
 }
 
-void unionNodes(int a,int b){
+void unionNodes(int a, int b){
 	int parent_a = findParent(a);
 	int parent_b = findParent(b);
+
 	if(parent_a == parent_b)
 		return;
 
-	if(sz[parent_a] >= sz[parent_b]){
-		swap(parent_a,parent_b);
+	if(sz[parent_a] > sz[parent_b]){
+		//a would be parent
+		parent[parent_b] = parent_a;
+		sz[parent_a] += sz[parent_b];
+	}
+	else{
+		//b would be parent
+		parent[parent_a] = parent_b;
+		sz[parent_b] += sz[parent_a];
 	}
 
-	sz[parent_b] += sz[parent_a];
-	parent[parent_a] = parent_b;
 	return;
 }
 
