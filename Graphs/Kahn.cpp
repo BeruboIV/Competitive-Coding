5ac31940c181fdda39bfe4e4bc816e9fb2d71bf3
BeruboIV <hardikraj13@gmail.com>
2021-02-01 00:00:40 +0530

    Added comments

--- Graphs/Kahn.cpp
+++ Graphs/Kahn.cpp
@@ -5,6 +5,7 @@ int in[N + 1];
 
 void kahn(int n){
 	queue<int> q;
+	//Use priority queue in case nodes have different priorities
 	for(int i = 1; i <= n; i++){
 		if(in[i] == 0)
 			q.push(i);
@@ -20,11 +21,18 @@ void kahn(int n){
 			}
 		}
 	}
+	// if(res.size() == n) topological sort exists else it does not exist
+
 }
 
 void solve(){
 	int n,m;
 	cin >> n >> m;
+	for(int i = 0; i <= n; i++){
+		adj[i].clear();
+		in[i] = 0;
+	}
+	res.clear();
 	for(int i = 0; i < m; i++){
 		int x,y;
 		cin >> x >> y;
