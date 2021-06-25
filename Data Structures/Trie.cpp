34f4ff1ebca7bb49f0e150f381a6a26c08c972fe
BeruboIV <hardikraj13@gmail.com>
2021-06-26 00:58:11 +0530

    Added remove string method

--- Data Structures/Trie.cpp
+++ Data Structures/Trie.cpp
@@ -47,10 +47,55 @@ bool search(Trie* root, string&str){
     return (temp->endOfWord);
 }
 
+bool isEmpty(Trie* root){
+    for(int i = 0; i < SIZE; i++){
+        if(root->children[i] != NULL)
+            return false;
+    }
+    return true;
+}
+
+// depth -> str.size()
+Trie* remove(Trie* root, string &str, int curr_depth, int depth){
+    // If the trie is empty
+    if(root == NULL)
+        return NULL;
+
+    // If last character is being processed
+    if(curr_depth == depth){
+        root->endOfWord = false;    // "unmark" the node
+
+        //  If the current node is not the prefix of any other node, we can delete this node
+        if(isEmpty(root)){
+            delete (root);
+            root = NULL;
+        }       
+        return root;
+    }
+
+    //  Recurse to the next letter/ node
+    int idx = str[curr_depth] - 'a';
+
+    // // If the node is not present
+    // if(root->children[idx] == NULL){
+    //     // Not needed as we are already checking root == NULL
+    //     return NULL;
+    // }
+
+    root->children[idx] = remove(root->children[idx], str, curr_depth + 1, depth);
+
+    if(isEmpty(root) && root->endOfWord == false){
+        delete (root);
+        root = NULL;
+    } 
+
+    return root;
+}
 
 void solve(){
     Trie* root = getNode();
     
     // Insert : insert(root, str);
     // Search : search(root, str);
+    // Remove : remove(root, str, 0, (int)str.size());
 }
