71237d8aaa2093eeee537e98cd611206d2e75017
BeruboIV <hardikraj13@gmail.com>
2021-07-04 16:43:43 +0530

    Optimized a little and changed caller function parameters

--- Graphs/Lowest Common Ancestor.cpp
+++ Graphs/Lowest Common Ancestor.cpp
@@ -23,14 +23,14 @@ void dfs(int node, int par){
 void precompute_LCA(int src, int n){
 	for(int i = 2; i <= N; i++)
 		LOG2[i] = LOG2[i/2] + 1;
-	int p = LOG2[n];
+	int p = LOG2[n];	// Small optimization
 	for(int i = 0; i <= n; i++){
 		for(int j = 0; j <= p; j++)
 			LCA[i][j] = -1;
 	}
 	dfs(src, -1);
-	for(int j = 1; j <= P; j++){
-		for(int i = 1; i <= N; i++){
+	for(int j = 1; j <= p; j++){
+		for(int i = 1; i <= n; i++){
 			int par = LCA[i][j-1];
 			if(par == -1)
 				LCA[i][j] = -1;
@@ -41,7 +41,7 @@ void precompute_LCA(int src, int n){
 	
 }
 
-int getLCA(int a, int b){
+int getLCA(int a, int b, int n){
 	//We need the deeper node
 	//Assume a is the deeper node
 	if(level[a] < level[b])
@@ -59,7 +59,8 @@ int getLCA(int a, int b){
 	// 	a = LCA[a][0];
 	// 	b = LCA[b][0];
 	// } Takes O(N) time
-	for(int i = P; i >= 0; i--){
+	int p = LOG2[n];	// Small optimization
+	for(int i = p; i >= 0; i--){
 		if(LCA[b][i] != -1 && (LCA[a][i] != LCA[b][i])){
 			a = LCA[a][i];
 			b = LCA[b][i];
@@ -71,18 +72,18 @@ int getLCA(int a, int b){
 void solve(){
 	int n;
 	cin >> n;
-	for(int i = 0; i < n - 1; i++){
+	for(int i = 1; i < n; i++){
 		int x,y;
 		cin >> x >> y;
 		adj[x].push_back(y);
 		adj[y].push_back(x);
 	}
-	precompute_LCA(n);
+	precompute_LCA(1, n);
 	int q;
 	cin >> q;
 	while(q--){
 		int a,b;
 		cin >> a >> b;
-		cout << getLCA(a,b) << "\n";
+		cout << getLCA(a, b, n) << "\n";
 	}
 }
