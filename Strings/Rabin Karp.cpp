e2e92e8e10cbcf8b0289297a9f117802c330bccf
BeruboIV <hardikraj13@gmail.com>
2021-07-10 17:40:28 +0530

    Update variable names

--- Strings/Rabin Karp.cpp
+++ Strings/Rabin Karp.cpp
@@ -1,25 +1,27 @@
-vector<int> rabin_karp(string const& s, string const& t) {
-    const long long P = 31; 
+vector<int> rabin_karp(string const& t, string const& p) {
+    // t -> text string
+    // p -> pattern string
+    const long long prime = 31; 
     const long long MOD = 1e9 + 7;
-    int S = s.size(), T = t.size();
+    int T = t.size(), P = p.size();
 
-    vector<long long> prime_pow(max(S, T)); 
+    vector<long long> prime_pow(max(T, P)); 
     prime_pow[0] = 1; 
     int n = prime_pow.size();
     for (int i = 1; i < n; i++) 
-        prime_pow[i] = (prime_pow[i - 1] * P) % MOD;
+        prime_pow[i] = (prime_pow[i - 1] * prime) % MOD;
 
-    vector<long long> h(T + 1, 0); 
+    vector<long long> hash_val(T + 1, 0); 
     for (int i = 0; i < T; i++)
-        h[i + 1] = (h[i] + (t[i] - 'a' + 1) * prime_pow[i]) % MOD; 
-    long long h_s = 0; 
-    for (int i = 0; i < S; i++) 
-        h_s = (h_s + (s[i] - 'a' + 1) * prime_pow[i]) % MOD; 
+        hash_val[i + 1] = (hash_val[i] + (t[i] - 'a' + 1) * prime_pow[i]) % MOD; 
+    long long hash_p = 0; 
+    for (int i = 0; i < P; i++) 
+        hash_p = (hash_p + (p[i] - 'a' + 1) * prime_pow[i]) % MOD; 
 
     vector<int> occurences;
-    for (int i = 0; i + S - 1 < T; i++) { 
-        long long cur_h = (h[i + S] - h[i] + MOD) % MOD; 
-        if (cur_h == h_s * prime_pow[i] % MOD)
+    for (int i = 0; i + P - 1 < T; i++) { 
+        long long cur_hash = (hash_val[i + P] - hash_val[i] + MOD) % MOD; 
+        if (cur_hash == hash_p * prime_pow[i] % MOD)
             occurences.push_back(i);
     }
     return occurences;
