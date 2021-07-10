6bd2a9a95ac54739e306f379367a155a08932bc5
BeruboIV <hardikraj13@gmail.com>
2021-07-10 17:24:56 +0530

    Changed iterating varaibles and names

--- Strings/KMP.cpp
+++ Strings/KMP.cpp
@@ -1,50 +1,50 @@
-vector<int> pi_table(string t){
-	int n = t.length();
-	vector<int> pi(n,0);
-	int i = 1, j = 0;
-	while(i < n){
-		if(t[i] == t[j]){
-			pi[i] = j + 1;
-			j++;
-			i++;
-		}
-		else{
-			if(j != 0)
-				j = pi[j - 1];
-			else{
-				i++;
-			}
-		}
-	}
-	return pi;
+vector<int> pi_table(const string &p){
+    int n = p.size();
+    vector<int> pi(n,0);
+    int i = 0, j = 1;
+    while(j < n){
+        if(p[i] == p[j]){
+            pi[j] = i + 1;
+            j++;
+            i++;
+        }
+        else{
+            if(i != 0)
+                i = pi[i - 1];
+            else{
+                j++;
+            }
+        }
+    }
+    return pi;
 }
 
-ll kmp(string s, string t){
-	// s --> The text string(is the string which we look into for the pattern)
-     //t --> Pattern String(is the pattern to be looked for in the string s)
-	 vector<int> pi = pi_table(t);
-     ll cnt = 0;
+long long kmp(const string &t, const string &p){
+    //  t --> The text string(is the string which we look into for the pattern)
+    //  p --> Pattern String(is the pattern to be looked for in the string s)
+     vector<int> pi = pi_table(p);
+     long long cnt = 0;
      int j = 0, i = 0;
-     int n = s.length();
-     int m = t.length();
-     while(i < n){
-     	if(s[i] == t[j]){
-     		i++;
-     		j++;
-     	}
-     	if(j == m){
-     		cnt++;
-     		j = 0; // For non-overlapping substrings
-     		//j = pi[j-1]; // For over-lapping substrings
-     	}
-     	else if(i < n && s[i] != t[j]){
-     		if(j != 0){
-     			j = pi[j - 1];
-     		}
-     		else{
-     			i++;
-     		}
-     	}
+     int m = t.size();
+     int n = p.size();
+     while(j < m){
+         if(t[j] == p[i]){
+             i++;
+             j++;
+         }
+         if(i == n){
+             cnt++;
+             i = 0; // For non-overlapping substrings
+             // i = pi[i-1]; // For over-lapping substrings
+         }
+         else if(j < n && t[j] != p[i]){
+             if(i != 0){
+                 i = pi[i - 1];
+             }
+             else{
+                 j++;
+             }
+         }
      }
      return cnt;
 }
