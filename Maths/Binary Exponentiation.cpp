29ed474f8e5fa07c191ed9135d6080225d58fa0f
BeruboIV <hardikraj13@gmail.com>
2021-08-11 13:32:03 +0530

    Add faster modulo arithmetic

--- Maths/Binary Exponentiation.cpp
+++ Maths/Binary Exponentiation.cpp
@@ -24,3 +24,35 @@ long long binpow(long long x,long long n, long long M){
     }
     return result;
 }
+
+// Faster Method
+int32_t mul(int64_t a, int32_t b){
+    return a * b % M;
+}
+
+int32_t add(int32_t a, int32_t b){
+    int32_t res = a + b;
+    if(res >= M)
+        res -= M;
+    return res;
+}
+
+int32_t sub(int32_t a, int32_t b){
+    int32_t res = a - b;
+    if(res < 0)
+        res += M;
+    return res;
+}
+
+int32_t binpow(int32_t x, int64_t n)
+{
+    int64_t result = 1;
+    while(n > 0)
+    {
+        if(n & 1)
+            result = mul(result, x);
+        x = mul(x, x);
+        n >>= 1;
+    }
+    return result;
+}
