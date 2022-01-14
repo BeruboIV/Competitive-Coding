08971f3dc31edd81c855fbea958141ca9f2e7f02
BeruboIV <hardikraj13@gmail.com>
2022-01-14 16:09:48 +0530

    Removed reduncdancies

--- Data Structures/RMQ (Minimum).cpp
+++ Data Structures/RMQ (Minimum).cpp
@@ -1,14 +1,15 @@
+
+// Note: 0 based indexing is followed here
+template <typename T = long long>
 class RMQ{
 
 public:
-    //0 Based indexing is followed here.
-    int N;
+    int N = 2e5;
     vector<int> LOG2;    //Quick lookup table for floor(log2(i)), 1 <= i <= N
-    vector<int64_t> dp[26];    //dp[i][j] = [j, j + (1 << i) ) --> Notice the open interval
-    vector<int64_t> arr;
+    vector<T> dp[26];    //dp[i][j] = [j, j + (1 << i) ) --> Notice the open interval
+    vector<T> arr;
 
     RMQ(){
-        N = 2e5;
         LOG2.resize(N + 1);
         arr.resize(N + 1);
         for(int i = 0; i < 26; i++){
@@ -25,15 +26,14 @@ public:
         arr.resize(N + 1);
         for(int i = 0; i < 26; i++){
             dp[i].resize(N + 1);
-            min_idx[i].resize(N + 1);
         }
         
         for(int i = 2; i <= N; i++)
             LOG2[i] = 1 + LOG2[i/2];
     }
 
-    RMQ (vector<int64_t> &a){
-        N = 2e5;
+    RMQ (vector<T> &a){
+        N = (a.empty() ? N : a.size());
         LOG2.resize(N + 1);
         arr.resize(N + 1);
         for(int i = 0; i < 26; i++){
@@ -47,7 +47,7 @@ public:
             build(a);
     }
 
-    void build(vector<int64_t> &a){
+    void build(vector<T> &a){
         int n = a.size();
         for(int i = 0; i < n; i++)
             arr[i] = a[i];
@@ -61,8 +61,8 @@ public:
         int P = LOG2[n];
         for(int p = 1; p <= P; p++){
             for(int i = 0; i + (1 << p) <= n; i++){
-                int64_t left = dp[p - 1][i];
-                int64_t right = dp[p - 1][i + (1 << (p - 1))];
+                T left = dp[p - 1][i];
+                T right = dp[p - 1][i + (1 << (p - 1))];
                 if(arr[left] < arr[right])
                     dp[p][i] = left;
                 else
@@ -73,19 +73,22 @@ public:
     }
 
     //Query the smallest element in the range [l, r], O(1)
-    int64_t query_val(int l, int r){
+    T query_val(int l, int r){
+        if(l > r){
+            return 1e18;
+        }
         int len = r - l + 1;
         int p = LOG2[len];
-        int64_t left = arr[dp[p][l]];
-        int64_t right = arr[dp[p][r - (1 << p) + 1]];
+        T left = arr[dp[p][l]];
+        T right = arr[dp[p][r - (1 << p) + 1]];
         return min(left, right);
     }
 
     //Query the smallest element in the range [l, r] by doing
     //a cascading min query, O(log2(n));
     //This function can be used to return sum of ranges also
-    int64_t CascadingQuery(int l, int r){
-        int64_t min_val = 1e18;
+    T CascadingQuery(int l, int r){
+        T min_val = 1e18;
         for(int p = LOG2[r - l + 1]; l <= r; p = LOG2[r - l + 1]){
             min_val = min(min_val, arr[dp[p][l]]);
             l += (1 << p);
@@ -94,6 +97,9 @@ public:
     }
 
     int query_idx(int l, int r){
+        if(l > r){
+            return N + 1;
+        }
         int len = r - l + 1;
         int p = LOG2[len];
         int left = dp[p][l];
@@ -107,11 +113,11 @@ void solve(){
     int n;
     cin >> n;
 
-    vector<int64_t> arr(n);
+    vector<long long> arr(n);
     for(auto &x : arr)
         cin >> x;
 
-    RMQ rmq(arr);
+    RMQ<long long> rmq(arr);
 
     int q;
     cin >> q;
