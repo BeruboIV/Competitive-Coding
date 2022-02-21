9c3de371ff929f7440fa4c1c09d03d96dc8713b9
BeruboIV <hardikraj13@gmail.com>
2022-02-21 15:29:26 +0530

    Rename variables

--- Graphs/Kahn.cpp
+++ Graphs/Kahn.cpp
@@ -1,13 +1,13 @@
 const int N = 5e5;
 vector<int> adj[N + 1];
 vector<int> res;
-int in[N + 1];
+int in_degree[N + 1];
 
 void kahn(int n){
 	queue<int> q;
 	//Use priority queue in case nodes have different priorities
 	for(int i = 1; i <= n; i++){
-		if(in[i] == 0)
+		if(in_degree[i] == 0)
 			q.push(i);
 	}
 	while(!q.empty()){
@@ -15,13 +15,13 @@ void kahn(int n){
 		q.pop();
 		res.push_back(curr);
 		for(int node : adj[curr]){
-			in[node]--;
-			if(in[node] == 0){
+			in_degree[node]--;
+			if(in_degree[node] == 0){
 				q.push(node);
 			}
 		}
 	}
-	// if(res.size() == n) topological sort exists else it does not exist
+	// return (res.size() == n);	// topological sort exists else it does not exist
 
 }
 
@@ -30,14 +30,14 @@ void solve(){
 	cin >> n >> m;
 	for(int i = 0; i <= n; i++){
 		adj[i].clear();
-		in[i] = 0;
+		in_degree[i] = 0;
 	}
 	res.clear();
 	for(int i = 0; i < m; i++){
 		int x,y;
 		cin >> x >> y;
 		adj[x].push_back(y);
-		in[y]++;
+		in_degree[y]++;
 	}
 	kahn(n);
 }
