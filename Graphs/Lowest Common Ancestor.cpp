7ff1b66eb1bf3702580a5e3e2a2ff6c38e1bc985
BeruboIV <hardikraj13@gmail.com>
2022-05-08 21:44:11 +0530

    Working Code

--- Graphs/Lowest Common Ancestor.cpp
+++ Graphs/Lowest Common Ancestor.cpp
@@ -1,17 +1,24 @@
-// Class based LCA
-// 1 Based indexing is used
+// CLASS BASED LCA
 
+// 0 Based indexing is used        
 template <typename T = long long>
 class LCA{
 public:
     int N = 2e5;    //Number of nodes.
     int P = 25; //Highest set bit possible
-    int LOG2[N + 1], level[N + 1];
-    int LCA[N + 1][P + 2]; //LCA[i][j] stores the 2^(jth) parent of node i
-    vector<int> graph[N + 1];
+    vector<int> LOG2, level;
+    vector<vector<int>> lca;    //LCA[i][j] stores the 2^(jth) parent of node i
+    vector<vector<int>> graph;
 
     LCA(int n = 200005){
-        N = n + 1;
+        N = n;
+        LOG2.resize(N + 10);
+        level.resize(N + 10);
+        lca.resize(N + 10);
+        graph.resize(N + 10);
+        for(int i = 0; i <= N; i++){
+            lca[i].resize(27);
+        }
     }
 
     void add_edge(int x, int y){
@@ -19,13 +26,14 @@ public:
         graph[y].push_back(x);
     }
 
-    void dfs(int node, int par, int depth = 0){
+    void dfs(int node, int par = -1, int depth = 0){
+        lca[node][0] = par;
+        level[node] = depth;
+
 
         for(int child : graph[node]){
             if(child == par)
                 continue;
-            LCA[child][0] = node;   // Parent
-            level[child] = depth + 1;
             dfs(child, node, depth + 1);
         }
     }
@@ -37,19 +45,18 @@ public:
         int p = LOG2[N];
         for(int i = 0; i <= N; i++){
             for(int j = 0; j <= p; j++){
-                LCA[i][j] = -1;
+                lca[i][j] = -1;
             }
         }
 
-        level[src] = 0;
         dfs(src, -1, 0);
         for(int p = 1; p <= P; p++){
-            for(int node = 1; node <= N; node++){
-                int par = LCA[node][p - 1];
+            for(int node = 0; node <= N; node++){
+                int par = lca[node][p - 1];
                 if(par == -1)
-                    LCA[node][p] = -1;
+                    lca[node][p] = -1;
                 else
-                    LCA[node][p] = LCA[par][p - 1];
+                    lca[node][p] = lca[par][p - 1];
             }
         }
     }
@@ -62,38 +69,36 @@ public:
         int diff = level[a] - level[b];
         while(diff > 0){
             int i = LOG2[diff];
-            a = LCA[a][i];
+            a = lca[a][i];
             diff -= (1 << i);
         }
         //If a is in the subtree of b
         if(a == b)
             return a;
-        // while(LCA[a][0] != LCA[b][0]){
-        //     a = LCA[a][0];
-        //     b = LCA[b][0];
+        // while(lca[a][0] != lca[b][0]){
+        //     a = lca[a][0];
+        //     b = lca[b][0];
         // } Takes O(N) time
         for(int i = P; i >= 0; i--){
-            if(LCA[b][i] != -1 && (LCA[a][i] != LCA[b][i])){
-                a = LCA[a][i];
-                b = LCA[b][i];
+            if(lca[b][i] != -1 && (lca[a][i] != lca[b][i])){
+                a = lca[a][i];
+                b = lca[b][i];
             }
         }
-        return LCA[a][0];
+        return lca[a][0];
     }
 };
 
 void solve(){
-    int n;
-    cin >> n;
-    LCA<long long> lca(n);
-    for(int i = 0; i < n - 1; i++){
-        int x,y;
-        cin >> x >> y;
-        lca.add_edge(x, y);
+    int n, q;
+    cin >> n >> q;
+    LCA<int> lca(n);
+    for(int i = 1; i <= n - 1; i++){
+        int p;
+        cin >> p;
+        lca.add_edge(i, p);
     }
-    lca.precompute_LCA(1);
-    int q;
-    cin >> q;
+    lca.precompute_LCA(0);
     while(q--){
         int a,b;
         cin >> a >> b;
