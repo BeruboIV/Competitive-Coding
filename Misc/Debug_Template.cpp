51d4a8be841e162fada9edd26ef1a5052c20c29e
BeruboIV <hardikraj13@gmail.com>
2022-10-16 13:09:34 +0530

    Updated the template

--- Misc/Debug_Template.cpp
+++ Misc/Debug_Template.cpp
@@ -1,4 +1,60 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////////
+/* 
+*   All function prototypes
+*/
+
+template<class T>
+void _print(T &x);
+
+template<class T>
+void _print(const T &x);
+
+template<class T>
+void _print(vector<T> &v1);
+
+template<class T>
+void _print(set<T> &s1);
+
+template<class T>
+void _print(multiset<T> &s1);
+
+template<class T, class V>
+void _print(map<T, V> &mp);
+
+template<class T, class V, class R>
+void _print(map<pair<T, V>, R> &mp);
+
+template<class T, class V>
+void _print(map<T, vector<V>> &mp);
+
+template<class T, class V>
+void _print(const pair<T, V> &p);
+
+template<class T, class V>
+void _print(pair<T, V> &p);
+
+template<class T>
+void _print(vector<vector<T>> &vec);
+
+template<class T, class V>
+void _print(vector<pair<T, V>> &vec);
+
+template<class T, class V>
+void _print(vector<vector<pair<T, V>>> &vec);
+
+template<class T, class V>
+void _print(set<pair<T, V>> &sett);
+
+template<class T, class V>
+void _print(multiset<pair<T, V>> &sett);
+
+template<class T>
+void _print(queue<T> q);
+
+/*
+*   All function definitions
+*/ 
+
 template<class T>
 void _print(T &x){
     cerr << x;
@@ -66,6 +122,47 @@ void _print(map<T, V> &mp){
     cerr << "]";
 }
 
+template<class T, class V, class R>
+void _print(map<pair<T, V>, R> &mp){
+    cerr << "[";
+    int n = mp.size();
+    for(auto p : mp){
+        cerr << "{";
+        _print(p.first);
+        cerr << ", ";
+        _print(p.second);
+        cerr << "}";
+        n--;
+        if(n == 0)
+            break;
+        cerr << ", ";
+    }
+    cerr << "]";
+}
+
+template<class T, class V>
+void _print(map<T, vector<V>> &mp){
+    cerr << "[";
+    int n = mp.size();
+    for(auto p : mp){
+        cerr << "{";
+        _print(p.first);
+        cerr << ": ";
+        _print(p.second);
+        cerr << "}";
+        n--;
+        if(n == 0)
+            break;
+        cerr << ", ";
+    }
+    cerr << "]";
+}
+
+template<class T, class V>
+void _print(const pair<T, V> &p){
+    cerr << "{" << p.first << ", " << p.second << "}";
+}
+
 template<class T, class V>
 void _print(pair<T, V> &p){
     cerr << "{" << p.first << ", " << p.second << "}";
@@ -99,6 +196,62 @@ void _print(vector<pair<T, V>> &vec){
     cerr << "]";
 }
 
+template<class T, class V>
+void _print(vector<vector<pair<T, V>>> &vec){
+    cerr << "[";
+    int n = vec.size();
+    for(vector<pair<T, V>> &vec1 : vec){
+        _print(vec1);
+        n--;
+        if(n == 0)
+            break;
+        cerr << ", ";
+    }
+    cerr << "]";
+}
+
+template<class T, class V>
+void _print(set<pair<T, V>> &sett){
+    cerr << "[";
+    int n = sett.size();
+    for(const pair<T, V> &p : sett){
+        _print(p);
+        n--;
+        if(n == 0)
+            break;
+        cerr << ", ";
+    }
+    cerr << "]";
+}
+
+template<class T, class V>
+void _print(multiset<pair<T, V>> &sett){
+    cerr << "[";
+    int n = sett.size();
+    for(const pair<T, V> &p : sett){
+        _print(p);
+        n--;
+        if(n == 0)
+            break;
+        cerr << ", ";
+    }
+    cerr << "]";
+}
+
+template<class T>
+void _print(queue<T> q){
+    cerr << "[";
+    int n = q.size();
+    while(n--){
+        _print(q.front());
+        q.pop();
+        if(n == 0)
+            break;
+        cerr << ", ";
+    }
+    cerr << "]";
+}
+
 vector<string> vec_splitter(string s) {
     vector<string> res;
     s += ',';
@@ -119,7 +272,7 @@ void _Print(T var1, Types&&... var2){
     int i = 0;
 
     auto Print_recursive = [&](auto& self, auto&& h, auto&&... var2) {
-        // std::cout << sizeof...(var2) << "\n";
+        // std::cout << sizeof...(var2) << endl;
 
         if constexpr ( sizeof...(var2) >= 0 ){
             cerr << res[i] << " = ";
