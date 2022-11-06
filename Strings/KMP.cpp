882d6fa275ac93f1b00b5973ae7478d026c822eb
BeruboIV <hardikraj13@gmail.com>
2022-11-06 18:18:37 +0530

    Updated the code using Galen Colin's and CP-Algorithms blog and

--- Strings/KMP.cpp
+++ Strings/KMP.cpp
@@ -1,50 +1,36 @@
-vector<int> pi_table(const string &p){
-    int n = p.size();
-    vector<int> pi(n,0);
-    int i = 0, j = 1;
-    while(j < n){
-        if(p[i] == p[j]){
-            pi[j] = i + 1;
+vector<int> prefix_function(const string &s) {
+    int n = (int)s.length();
+    vector<int> pi(n, 0);
+    pi[0] = 0;
+
+    for (int i = 1; i < n; i++) {
+        pi[i] = 0;
+        int j = pi[i - 1];    /* trying length j + 1 */
+        while (j > 0 && s[i] != s[j])
+            j = pi[j - 1];
+        if (s[i] == s[j])
             j++;
-            i++;
-        }
-        else{
-            if(i != 0)
-                i = pi[i - 1];
-            else{
-                j++;
-            }
-        }
+        pi[i] = j;
     }
     return pi;
 }
 
-long long kmp(const string &t, const string &p){
-    //  t --> The text string(is the string which we look into for the pattern)
-    //  p --> Pattern String(is the pattern to be looked for in the string s)
-     vector<int> pi = pi_table(p);
-     long long cnt = 0;
-     int j = 0, i = 0;
-     int m = t.size();
-     int n = p.size();
-     while(j < m){
-         if(t[j] == p[i]){
-             i++;
-             j++;
-         }
-         if(i == n){
-             cnt++;
-             i = 0; // For non-overlapping substrings
-             // i = pi[i-1]; // For over-lapping substrings
-         }
-         else if(j < n && t[j] != p[i]){
-             if(i != 0){
-                 i = pi[i - 1];
-             }
-             else{
-                 j++;
-             }
-         }
-     }
-     return cnt;
+
+vector<int> kmp_find_matches(const string &text, const string &pat){
+    //  text --> The text string(is the string which we look into for the pattern)
+    //  pat --> Pattern String(is the pattern to be looked for in the text string "text")
+    int n = pat.length(), m = text.length();
+    string s = pat + "$" + text;
+    vector<int> pi = prefix_function(s), ans;
+
+    for (int i = n + 1; i <= n + m; i++) { /* n + 1 is where the text starts */
+        if (pi[i] == n) {
+            ans.push_back(i - 2 * n); /* i - (n - 1) - (n + 1) */
+        }
+    }
+    
+    return ans;
 }
+
+// Question 1: https://www.spoj.com/problems/NAJPF/
+// Solution 1: https://ideone.com/pKifdk
