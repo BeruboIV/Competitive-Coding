5abf1606ee0e780ebaf9bc8a1dd1034904c60c23
BeruboIV <hardikraj13@gmail.com>
2022-12-03 00:15:25 +0530

    Changed to struct based

--- Data Structures/DSU.cpp
+++ Data Structures/DSU.cpp
@@ -1,82 +1,99 @@
-const int N = 6e5 + 10;
-int parent[N], sz[N];
+struct DSU {
+public:
+    vector<int> parent, sz;
+    int N;
 
-/* If parent[i] = -1 for all i <= N
-int findParent(int a){
-	vector<int> nodes;
-	while(parent[a] > 0){
-		nodes.push_back(a);
-		a = parent[a];
-	}
+    /* If parent[i] = -1 for all i <= N
+    int findParent(int a){
+        vector<int> nodes;
+        while(parent[a] > 0){
+            nodes.push_back(a);
+            a = parent[a];
+        }
 
-	for(int node : nodes)
-		parent[node] = a;
-	return a;
-}
+        for(int node : nodes)
+            parent[node] = a;
+        return a;
+    }
 
-OR
+    OR
 
-int findParent(int a){
-	if(parent[a] == -1)
-		return a;
-	return parent[a] = findParent(parent[a]);
-}
-*/
+    int findParent(int a){
+        if(parent[a] == -1)
+            return a;
+        return parent[a] = findParent(parent[a]);
+    }
+    */
 
-int findParent(int i){
-	if(parent[parent[i]] != parent[i])
-		parent[i] = findParent(parent[i]);
-	return parent[i];
-}
+    DSU(int n){
+        N = n;
+        parent.resize(n + 1);
+        sz.resize(n + 1);
 
-void unionNodes(int a, int b){
-	int parent_a = findParent(a);
-	int parent_b = findParent(b);
+        for(int i = 0; i <= n; i++){
+            parent[i] = i;
+            sz[i] = i;
+        }
+    }
 
-	if(parent_a == parent_b)
-		return;
+    int findParent(int i){
+        if(parent[parent[i]] != parent[i])
+            parent[i] = findParent(parent[i]);
+        return parent[i];
+    }
 
-	if(sz[parent_a] > sz[parent_b]){
-		//a would be parent
-		parent[parent_b] = parent_a;
-		sz[parent_a] += sz[parent_b];
-	}
-	else{
-		//b would be parent
-		parent[parent_a] = parent_b;
-		sz[parent_b] += sz[parent_a];
-	}
+    void unionNodes(int a, int b){
+        int parent_a = findParent(a);
+        int parent_b = findParent(b);
 
-	return;
-}
+        if(parent_a == parent_b)
+            return;
 
-void clearDSU(int n){
-	for(int i = 0; i <= n; i++){
-		parent[i] = i;
-		sz[i] = 1;
-	}
-}
+        if(sz[parent_a] > sz[parent_b]){
+            //a would be parent
+            parent[parent_b] = parent_a;
+            sz[parent_a] += sz[parent_b];
+        }
+        else{
+            //b would be parent
+            parent[parent_a] = parent_b;
+            sz[parent_b] += sz[parent_a];
+        }
+
+        return;
+    }
+
+    void clearDSU(int n){
+        for(int i = 0; i <= n; i++){
+            parent[i] = i;
+            sz[i] = 1;
+        }
+    }
+};
 
 void solve(){
-	int n;
-	cin >> n;
-	clearDSU(n);
-	int q;
-	cin >> q;
-	while(q--){
-		int a, b;
-		cin >> a >> b;
-		unionNodes(a, b);
-	}
-	for(int i = 1; i <= n; i++)
-		cout << parent[i] << " ";
-	cout << "\n";
-	// For printing the size of each component
-	for(int i = 1; i <= n; i++){
-		int p = parent[i];
-		cout << sz[p] << " ";
-	}
+    int n;
+    cin >> n;
+    DSU UF(n);
+    int q;
+    cin >> q;
+    while(q--){
+        int a, b;
+        cin >> a >> b;
+        UF.unionNodes(a, b);
+    }
+    for(int i = 1; i <= n; i++)
+        cout << UF.parent[i] << " ";
+    cout << "\n";
+    // For printing the size of each component
+    for(int i = 1; i <= n; i++){
+        int p = UF.findParent(i);
+        cout << UF.sz[p] << " ";
+    }
 }
 
 // Question: https://www.codechef.com/problems/CNCTCT?tab=statement
 // Solution: https://www.codechef.com/viewsolution/81320820
+
+// Question: https://leetcode.com/problems/checking-existence-of-edge-length-limited-paths/
+// Solution: https://leetcode.com/submissions/detail/853542036/
